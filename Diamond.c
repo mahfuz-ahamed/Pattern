1f8595dbf52127a3eb0a6f7a327400dad1d32c47
Mahfuz Ahamed Pallab <mahfuzahamed5737@gmail.com>
2025-12-01 08:58:05 +0600

    Add files via upload

---
+++ Diamond.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+int n=4;
+for(int i=1;i<=n;i++){
+    for(int s=1;s<=n-i;s++) printf(" ");
+    for(int j=1;j<=i;j++) printf("* ");
+    printf("\n");
+}
+for(int i=n-1;i>=1;i--){
+    for(int s=1;s<=n-i;s++) printf(" ");
+    for(int j=1;j<=i;j++) printf("* ");
+    printf("\n");
+}
+
+    return 0;
+}
