9d9efdd805af33556da3270aa3209e735fc95857
Mahfuz Ahamed Pallab <mahfuzahamed5737@gmail.com>
2025-12-08 09:22:49 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    printf("     ");
+    for (i = 1; i <= 10; i++) {
+        printf("%4d", i);
+    }
+  
+
+    for (i = 1; i <= 10; i++) {
+        printf("%2d |", i);
+        for (j = 1; j <= 10; j++) {
+            printf("%4d", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
